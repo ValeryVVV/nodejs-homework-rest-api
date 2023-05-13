61e718be457565229276d18cfac37e1187684a24
ValeryVVV <110850242+ValeryVVV@users.noreply.github.com>
2023-05-13 21:27:27 +0300

    added env file

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Lera:PcFu2QHYEtGxB7Uk@cluster0.ahlpiwc.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
